6f5bacf5776aaf32592d448e25e9396c41bf85f2
Macintosh <macintosh@Macintoshs-iMac.local>
2017-08-22 15:33:05 +0530

    initial commit

--- RECL/ViewController.swift
+++ RECL/ViewController.swift
@@ -9,17 +9,95 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var webView = UIWebView()
+    let url = URL(string: "http://125.22.91.10:8080/")
+    
+    let loader = UIActivityIndicatorView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupWebView()
+        openLoadRequest(url: url!)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func openLoadRequest(url: URL) {
+        let request = URLRequest(url: url)
+        webView.loadRequest(request)
+        
+    }
+    
+    func setupWebView() {
+        webView = UIWebView(frame: UIScreen.main.bounds)
+        view.addSubview(webView)
+        webView.delegate = self
+    }
+    
+    func showLoading() {
+        loader.center = self.view.center
+        loader.activityIndicatorViewStyle = .gray
+        view.addSubview(loader)
+        loader.startAnimating()
+        
+    }
+    
+    func stopLoading() {
+        self.loader.stopAnimating()
+        
+    }
 }
 
+extension ViewController: UIWebViewDelegate {
+    
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        if let url = request.url, navigationType == .linkClicked {
+                
+                if let scheme = url.scheme {
+                    openAppWithUrlScheme(scheme: scheme, url: url)
+                }
+                openLoadRequest(url: url)
+                return false
+        }
+        return true
+        
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        showLoading()
+    }
+    
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        stopLoading()
+    }
+    
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        print(error.localizedDescription)
+        
+    }
+    
+    
+    func openAppWithUrlScheme(scheme: String, url: URL) {
+        if scheme == "tel" {
+            validateToOpenUrl(url: url)
+            
+        } else if scheme == "mailto" {
+            validateToOpenUrl(url: url)
+        } else {
+            
+        }
+    }
+    
+    
+    func validateToOpenUrl(url: URL) {
+        UIApplication.shared.openURL(url)
+    }
+}
