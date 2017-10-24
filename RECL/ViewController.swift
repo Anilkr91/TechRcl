39437c91369a823fcdc11276a3aa2477249f17c1
admin <admin@Viveks-MacBook-Pro.local>
2017-10-24 16:06:23 +0530

    All users resolved

--- RECL/ViewController.swift
+++ RECL/ViewController.swift
@@ -10,45 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var webView = UIWebView()
+    @IBOutlet weak var webView: UIWebView!
     let url = URL(string: "http://125.22.91.10:8080/")
     
     let loader = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupWebView()
+        webView.delegate = self
         openLoadRequest(url: url!)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func openLoadRequest(url: URL) {
         let request = URLRequest(url: url)
         webView.loadRequest(request)
-        
-    }
-    
-    func setupWebView() {
-        webView = UIWebView(frame: UIScreen.main.bounds)
-        view.addSubview(webView)
-        webView.delegate = self
-    }
-    
-    func showLoading() {
-        loader.center = self.view.center
-        loader.activityIndicatorViewStyle = .gray
-        view.addSubview(loader)
-        loader.startAnimating()
-        
-    }
-    
-    func stopLoading() {
-        self.loader.stopAnimating()
         
     }
 }
@@ -58,32 +33,70 @@ extension ViewController: UIWebViewDelegate {
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         
         if let url = request.url, navigationType == .linkClicked {
-                
-                if let scheme = url.scheme {
-                    openAppWithUrlScheme(scheme: scheme, url: url)
+            
+            print(url)
+            
+            if let scheme = url.scheme   {
+                openAppWithUrlScheme(scheme: scheme, url: url)
+            }
+            
+            if url.absoluteString.contains("#") {
+                if url.absoluteString.contains("dashboard") {
+                    return true
+               
+                } else {
+                    onBackPressed()
                 }
+                
+            } else if url.absoluteString.contains("maps/place/") {
+                showBackButton()
+                print("map")
+                
+            } else if url.absoluteString.contains("facebook.com/") {
+                showBackButton()
+                print("facebook")
+                
+            } else if url.absoluteString.contains("youtube.com/") {
+                showBackButton()
+                print("youtube")
+                
+            } else if url.absoluteString.contains("twitter.com/") {
+                showBackButton()
+                print("twitter")
+                
+            } else if url.absoluteString.contains("linkedin.com/") {
+                showBackButton()
+                print("linkedin")
+                
+            } else {
                 openLoadRequest(url: url)
                 return false
+            }
         }
         return true
-        
+    }
+    
+    func onBackPressed() {
+        print("back pressed")
+        hideBackButton()
+        webView.goBack()
     }
     
     func webViewDidStartLoad(_ webView: UIWebView) {
-        showLoading()
+        ProgressBarView.showHUD()
     }
-    
     
     func webViewDidFinishLoad(_ webView: UIWebView) {
-        stopLoading()
+        ProgressBarView.hideHUD()
     }
-    
     
     func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
-        print(error.localizedDescription)
         
+        let alert = UIAlertController(title: error.localizedDescription, message: "", preferredStyle: .alert)
+        let dismiss = UIAlertAction(title: "OK", style: .default, handler: { action in ProgressBarView.hideHUD() })
+        alert.addAction(dismiss)
+        self.present(alert, animated: true, completion: nil)
     }
-    
     
     func openAppWithUrlScheme(scheme: String, url: URL) {
         if scheme == "tel" {
@@ -92,12 +105,20 @@ extension ViewController: UIWebViewDelegate {
         } else if scheme == "mailto" {
             validateToOpenUrl(url: url)
         } else {
-            
+            return
         }
     }
     
-    
     func validateToOpenUrl(url: URL) {
         UIApplication.shared.openURL(url)
+    }
+    
+    func showBackButton() {
+        let backButton = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(ViewController.onBackPressed))
+        self.navigationItem.leftBarButtonItem = backButton
+    }
+    
+    func hideBackButton() {
+        self.navigationItem.leftBarButtonItem = nil
     }
 }
